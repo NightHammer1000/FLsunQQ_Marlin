699d8befc5ef8ba91a0b17a1e6b89471db3bb53c
NightStorm1000 <nightstorm@kpc.bz>
2018-07-13 20:58:48 +0200

    Turn down ACCELERATION, JERK and FEEDRATE to get rid of ghosting

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -685,7 +685,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 500, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 300, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -693,7 +693,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 3000, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 1000, 2000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -703,9 +703,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          2000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -715,10 +715,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
-#define DEFAULT_ZJERK                 10.0 // Must be same as XY for delta
-#define DEFAULT_EJERK                  5.0
+#define DEFAULT_XJERK                 4.0
+#define DEFAULT_YJERK                 4.0
+#define DEFAULT_ZJERK                 4.0 // Must be same as XY for delta
+#define DEFAULT_EJERK                  2.0
 
 /**
  * S-Curve Acceleration
